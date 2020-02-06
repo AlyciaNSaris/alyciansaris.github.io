9300a9bd2bd2e89185edddcdff55ede00d466f0e
AlyciaNSaris <31761485+AlyciaNSaris@users.noreply.github.com>
2020-02-06 01:47:11 -0700

    2/6/2020 HTML
    2/6/2020 HTML5/CSS code only original version of website

--- nmb.php
+++
@@ -1,6 +0,0 @@
-<?php
-$WeatherSource = "https://api.sunrise-sunset.org/json?lat=33.4484376&lng=-112074036";
-header("Content-Type: application/json");
-header("Cache-Control: no-cache");
-readfile($WeatherSource);
-?>
